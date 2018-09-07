7d4f656f5722b1d9a0aa48a17fdb5ae25e88c5f3
Keller <381796751@qq.com>
2018-09-07 22:56:48 +0800

    任务5-脚本 主观题

---
+++ demo.sh
@@ -0,0 +1,13 @@
+if [ -d $1 ]; then
+  echo 'error: dir exists'
+  exit
+else
+  mkdir $1
+  cd $1
+  mkdir css js
+  echo -e "<!DOCTYPE>\n<title>Hello</title>\n<h1>Hi</h1>" > index.html
+  echo -e "h1{color: red;}" > css/style.css
+  echo -e "var string = \"Hello World\"\nalert(string)" > js/main.js
+  echo 'success'
+  exit
+fi
